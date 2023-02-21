e4ae7e9902f8f25f735aa05f028c6e9a51620cfc
Fikri Fauzy N <110448875+EFENNN@users.noreply.github.com>
2023-02-21 08:23:49 +0700

    Add files via upload

---
+++ Admin/CRUD-siswa.php
@@ -0,0 +1,28 @@
+    <?php
+    include "../template-spp/header.php";
+    include "../template-spp/navbar.php";
+    include "../template-spp/sidebar.php";
+    ?>
+    
+      
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="row">
+            <div class="col">
+              <div class="card card-statistic-2">
+                <div class="card-stats">
+                  <div class="card-stats-title text-center"><h2>Data siswa</h2>
+                </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </section>
+      </div>
+      
+      <?php
+      include "../template-spp/footer.php";
+      ?>
+
+  
---
+++ template-spp/sidebar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<!-- General CSS Files -->
+<link rel="stylesheet" href="../assets/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/modules/fontawesome/css/all.min.css">
+
+  <!-- CSS Libraries -->
+  <link rel="stylesheet" href="../assets/modules/jqvmap/dist/jqvmap.min.css">
+  <link rel="stylesheet" href="../assets/modules/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="../assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="../assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css">
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="../assets/css/components.css">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Pembayaran SPP</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">SPP</a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="active">
+              <a href="dashboard.php" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+          <li><a class="nav-link" href="CRUD-petugas.php"><i class="fas fa-address-book"></i> <span>CRUD PETUGAS</span></a></li>
+          <li><a class="nav-link" href="CRUD-siswa.php"><i class="fas fa-address-book"></i> <span>CRUD SISWA</span></a></li>
+          <li><a class="nav-link" href="CRUD-kelas.php"><i class="fas fa-address-book"></i> <span>CRUD KELAS</span></a></li>
+          <li><a class="nav-link" href="CRUD-spp.php"><i class="fas fa-address-book"></i> <span>CRUD SPP</span></a></li>
+          <li><a class="nav-link" href="credits.html"><i class="fas fa-dollar-sign"></i> <span>Entri Pembayaran</span></a></li>
+          <li><a class="nav-link" href="credits.html"><i class="fas fa-history"></i> <span>History Pembayaran</span></a></li>
+          
+          </ul>
+
+          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+              <i class="fas fa-rocket"></i>Log Out</a>
+          </div>        
+        </aside>
+      </div>
+</body>
+</html>
